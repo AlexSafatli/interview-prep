2635acd680c1b6e66f6f7b02377afe89ad0cefe2
Alex Safatli <safatli@cs.dal.ca>
2015-05-17 11:01:14 -0300

    Started adding Java code for CTCI code.

---
+++ CTCI/Java/LinkedList.java
@@ -0,0 +1,70 @@
+public class LinkedList<T> {
+	
+	private Node<T> head;
+	private int size;
+
+	public LinkedList() {
+		head = null;
+	}
+
+	public Node<T> getHead() {
+		return head;
+	}
+
+	public int getSize() {
+		return size;
+	}
+
+	public Node<T> getTail() {
+		Node<T> cursor = getHead();
+		if (cursor == null) return null;
+		while (cursor.getNext() != null) {
+			cursor = cursor.getNext();
+		}
+		return cursor;
+	}
+
+	public Node<T> appendToTail(T data) {
+		Node<T> n = new Node<T>(data);
+		appendNodeToTail(n);
+		return n;
+	}
+
+	public void appendNodeToTail(Node<T> node) {
+		if (head == null) head = node;
+		else {
+			Node<T> cursor = getTail();
+			cursor.setNext(node);
+		}
+		++size;
+	}
+
+	public void deleteNode(Node<T> node) {
+		Node<T> cursor = head;
+		if (cursor != null) {
+			if (cursor.equals(node)) {
+				head = cursor.getNext();
+				--size;
+				return;
+			}
+			while (cursor.getNext() != null) {
+				if (cursor.getNext().equals(node)) {
+					cursor.setNext(cursor.getNext().getNext());
+					--size;
+					return;
+				}
+				cursor = cursor.getNext();
+			}
+			throw new RuntimeException("Node not found in list.");
+		}
+	}
+
+	public static void main(String[] args) {
+		LinkedList<Integer> intList = new LinkedList<Integer>();
+		intList.appendToTail(2);
+		intList.appendToTail(10);
+		intList.deleteNode(intList.getTail());
+		System.out.println(intList.getSize());
+	}
+
+}
---
+++ CTCI/Java/Node.java
@@ -0,0 +1,36 @@
+public class Node<T> {
+	
+	private Node<T> next;
+	private T data;
+
+	public Node() {
+		next = null; data = null;
+	}
+
+	public Node(T d) {
+		next = null; data = d;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T d) {
+		data = d;
+	}
+
+	public Node<T> getNext() {
+		return next;
+	}
+
+	public void setNext(Node<T> node) {
+		next = node;
+	}
+
+	public boolean equals(Object other) {
+		Node<T> node = (Node<T>)other;
+		T odata = (T)node.getData();
+		return (odata.equals(data));
+	}
+
+}
