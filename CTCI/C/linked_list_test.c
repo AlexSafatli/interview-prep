9b3e2d9543e930124364b8f4d65296c60ff7068b
Alex Safatli <safatli@cs.dal.ca>
2015-10-17 15:58:09 -0300

    Modification to test

--- CTCI/C/linked_list_test.c
+++ CTCI/C/linked_list_test.c
@@ -4,12 +4,13 @@
 int main(void) {
   int a = 5;
   int b = 6;
+  int *ptr;
   linked_list_t* list = linked_list_init();
   linked_list_append_to_head(list, &a);
   linked_list_append_to_head(list, &b);
   linked_list_append_to_tail(list, &b);
   linked_list_append_to_tail(list, &b);
-  int *ptr;
+  linked_list_remove_tail(list, (void**) &ptr);
   while (linked_list_remove_head(list, (void**) &ptr)) {
     printf("%d ", *ptr);
   } printf("\n");
