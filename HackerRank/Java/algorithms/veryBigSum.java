3865be6a660edbffd62c8251086805c60de5e64b
Alex Safatli <safatli@cs.dal.ca>
2015-10-29 19:15:10 -0300

    Gao~

--- HackerRank/Java/algorithms/veryBigSum.java
+++ HackerRank/Java/algorithms/veryBigSum.java
@@ -1,11 +1,10 @@
 import java.util.*;
 
 class Solution {
-
-    public static void main(String[] args) {
-        Scanner stdin = new Scanner(System.in);
-        long sum = 0;
-        for (int n = stdin.nextInt(); n > 0; --n) sum += stdin.nextLong();
-        System.out.println(sum);
-    }
+  public static void main(String[] args) {
+    Scanner stdin = new Scanner(System.in);
+    long sum = 0;
+    for (int n = stdin.nextInt(); n > 0; --n) sum += stdin.nextLong();
+    System.out.println(sum);
+  }
 }
