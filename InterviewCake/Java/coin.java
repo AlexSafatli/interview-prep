f314e198773d7b6cbc60fa5cf1c9ae63c9a44557
Alex Safatli <safatli@cs.dal.ca>
2015-11-01 09:49:34 -0400

    Add DP coin question solution

---
+++ InterviewCake/Java/coin.java
@@ -0,0 +1,24 @@
+class Solution {
+
+  public static int numberOfWaysToMakeAmountWithDenominations(int amount,
+    int[] denominations) {
+    // waysForNCents[k] = how many ways to get to k cents using denominations
+    int[] waysForNCents = new int[amount + 1];
+    waysForNCents[0] = 1; // Always only a single way to make 0 cents.
+    for (int i = 0; i < denominations.length; ++i) {
+      int coin = denominations[i];
+      for (int higher = coin; higher < amount + 1; ++higher) {
+        waysForNCents[higher] += waysForNCents[higher - coin];
+      }
+    }
+    return waysForNCents[amount];
+  }
+
+  public static void main(String[] args) {
+    System.out.println(numberOfWaysToMakeAmountWithDenominations(4, 
+      new int[] { 1, 2, 3 }));
+    System.out.println(numberOfWaysToMakeAmountWithDenominations(5,
+      new int[] { 1, 3, 5 }));
+  }
+
+}
