2d2a5b1fd6457f9404eb8fc4193b1b799bb7e7f2
Alex Safatli <safatli@cs.dal.ca>
2015-11-02 13:03:12 -0400

    Some CTCI reading work

---
+++ CTCI/Java/DataStructures/StringBuffer.java
@@ -0,0 +1,25 @@
+package structures;
+import java.util.List;
+import java.util.ArrayList;
+
+public class StringBuffer {
+
+  private List<String> strings;
+
+  public StringBuffer() {
+    strings = new ArrayList<String>();
+  }
+
+  public StringBuffer(int n) {
+    strings = new ArrayList<String>(n);
+  }
+
+  public void append(String s) {
+    strings.add(s);
+  }
+
+  public String toString() {
+    return new String(strings.toArray(new String[strings.size()]));
+  }
+
+}
