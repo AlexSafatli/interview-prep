2275a6475f860a06a02b4f06b60b800b5e57e90f
Alex Safatli <safatli@cs.dal.ca>
2015-11-11 16:07:34 -0400

    Clean linked list header file in CTCI

--- CTCI/C/linked_list.h
+++ CTCI/C/linked_list.h
@@ -10,12 +10,12 @@ typedef struct {
   node_t *head;
 } linked_list_t;
 
-linked_list_t *linked_list_init ( );
-void linked_list_destroy ( linked_list_t *list );
-int linked_list_get ( linked_list_t *list, int pos, void **data );
-int linked_list_append_to_head ( linked_list_t *list, void *data );
-int linked_list_remove_head ( linked_list_t *list, void **data );
-int linked_list_append_to_tail ( linked_list_t *list, void *data );
-int linked_list_remove_tail ( linked_list_t *list, void **data );
+linked_list_t *linked_list_init();
+void linked_list_destroy(linked_list_t *list);
+int linked_list_get(linked_list_t *list, int pos, void **data);
+int linked_list_append_to_head(linked_list_t *list, void *data);
+int linked_list_remove_head(linked_list_t *list, void **data);
+int linked_list_append_to_tail(linked_list_t *list, void *data);
+int linked_list_remove_tail(linked_list_t *list, void **data);
 
-#endif /* _linked_list_h_ */
+#endif /*_linked_list_h_*/
