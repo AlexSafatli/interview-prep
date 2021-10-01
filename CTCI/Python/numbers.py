f9320051116693f5c64eb488cc55be7a3b8b4fbd
Alex Safatli <safatli@cs.dal.ca>
2021-10-01 17:25:08 -0300

    Add binary search implementation

--- CTCI/Python/numbers.py
+++ CTCI/Python/numbers.py
@@ -1,3 +1,6 @@
+import typing
+
+
 def fib(n: int) -> int:
     """Finds the nth Fibonacci number, iterative."""
     up, lo = 1, 0
@@ -33,9 +36,28 @@ def add_no_arithmetic_operators(a: int, b: int) -> int:
     return add_no_arithmetic_operators(sum_no_carry, carry)
 
 
+# Implementation of the binary search algorithm for a list of integers.
+def binary_search(arr: typing.List[int], key: int) -> int:
+    # Assumes already sorted array
+    lower = 0
+    upper = len(arr) - 1
+    mid = int((lower + upper) / 2)
+    while arr[mid] != key and lower < upper:
+        if key < arr[mid]:
+            upper = mid - 1
+        elif key > arr[mid]:
+            lower = mid + 1
+        mid = int((lower + upper) / 2)
+    if lower <= upper:  # number was found
+        return mid
+    return - 1  # was not found
+
+
 if __name__ == '__main__':
     print('fib(5):', fib(5))
     print('is_prime(7):', is_prime(7))
     print('is_prime(673):', is_prime(673))
     print('add_no_arithmetic_operators(5,8):',
           add_no_arithmetic_operators(5, 8))
+    numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 21, 25, 28]
+    print(numbers[binary_search(numbers, 25)])
