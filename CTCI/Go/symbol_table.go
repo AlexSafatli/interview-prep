b6d8100d6a218f2597983099a0c24f3e4f897901
Alex Safatli <safatli@cs.dal.ca>
2021-10-09 13:18:31 -0300

    Clear up remaining generic Go issues in CTCI

--- CTCI/Go/symbol_table.go
+++ CTCI/Go/symbol_table.go
@@ -1,71 +1,70 @@
 package main
 
-/* Node */
-type Node struct {
-  next *Node
-  data int
-  key int
+type SymbolTableNode struct {
+	next *SymbolTableNode
+	data int
+	key  int
 }
 
-func NewNode(key int, data int) *Node {
-  n := new(Node)
-  n.data = data
-  n.key = key
-  return n
+func NewSymbolTableNode(key int, data int) *SymbolTableNode {
+	n := new(SymbolTableNode)
+	n.data = data
+	n.key = key
+	return n
 }
 
-/* Linked List */
+// SymbolTable (Linked List)
 type SymbolTable struct {
-  head *Node
-  size int
+	head *SymbolTableNode
+	size int
 }
 
 func NewSymbolTable() *SymbolTable {
-  l := new(SymbolTable)
-  return l
+	l := new(SymbolTable)
+	return l
 }
 
-func (list *SymbolTable) GetTail() *Node {
-  if (list.head == nil) {
-    return nil
-  }
-  var cursor *Node = list.head
-  for ; cursor.next != nil ; {
-    cursor = cursor.next
-  }
-  return cursor
+func (list *SymbolTable) GetTail() *SymbolTableNode {
+	if list.head == nil {
+		return nil
+	}
+	var cursor = list.head
+	for cursor.next != nil {
+		cursor = cursor.next
+	}
+	return cursor
 }
 
 func (list *SymbolTable) Put(key int, data int) {
-  n := NewNode(key, data)
-  list.AppendNodeToTail(n)
+	n := NewSymbolTableNode(key, data)
+	list.AppendNodeToTail(n)
 }
 
-func (list *SymbolTable) AppendNodeToTail(node *Node) {
-  if (list.head == nil) {
-    list.head = node
-  } else {
-    list.GetTail().next = node
-  }
-  list.size++
+func (list *SymbolTable) AppendNodeToTail(node *SymbolTableNode) {
+	if list.head == nil {
+		list.head = node
+	} else {
+		list.GetTail().next = node
+	}
+	list.size++
 }
 
 func (list *SymbolTable) Get(key int) int {
-  if (list.head == nil) {
-    return -1
-  }
-  var cursor *Node = list.head
-  for ; cursor != nil ; cursor = cursor.next {
-    if (cursor.key == key) {
-      return cursor.data
-    }
-  }
-  return -1
+	if list.head == nil {
+		return -1
+	}
+	var cursor = list.head
+	for ; cursor != nil; cursor = cursor.next {
+		if cursor.key == key {
+			return cursor.data
+		}
+	}
+	return -1
 }
 
 /* Main (Test) */
 func main() {
-  l := NewSymbolTable()
-  l.Put(2, 2)
-  println(l.Get(2))
+	l := NewSymbolTable()
+	l.Put(2, 2)
+	println(l.Get(2))
 }
