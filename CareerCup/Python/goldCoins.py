1188851e7ef2b098f1b1ff8cda7d19ac2d6895c5
Alex Safatli <safatli@cs.dal.ca>
2021-10-12 15:05:38 -0300

    Convert goldCoins to gold_coints (py3)

---
+++ CareerCup/Python/goldCoins.py
@@ -0,0 +1,23 @@
+# http://www.dsalgo.com/2013/02/gold-coins-in-pots-game.html
+
+def number_of_coins(pots) -> int:
+    outcomes = [[None for _ in range(0, len(pots))] for _ in
+                range(0, len(pots))]
+    return max_coins(pots, 0, len(pots) - 1, outcomes)
+
+
+def max_coins(pots, lo, up, f) -> int:
+    if lo > up:
+        return 0
+    if f[lo][up] is not None:
+        return f[lo][up]
+    F = lambda i, j: max_coins(pots, i, j, f)
+    start = pots[lo] + min([F(lo + 2, up), F(lo + 1, up - 1)])
+    end = pots[up] + min([F(lo, up - 2), F(lo + 1, up - 1)])
+    f[lo][up] = max([start, end])
+    return f[lo][up]
+
+
+if __name__ == '__main__':
+    gold_pots = [12, 32, 4, 23, 6, 42, 16, 3, 85, 23, 4, 7, 3, 5, 45, 34, 2, 1]
+    print(gold_pots, number_of_coins(gold_pots))
