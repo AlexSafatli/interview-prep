70f9d9cee4eecedaa10ed93198450dca217afade
Alex Safatli <safatli@cs.dal.ca>
2021-10-14 22:49:23 -0300

    Add better graph implementation with get_path

--- CTCI/Python/graphs.py
+++ CTCI/Python/graphs.py
@@ -3,65 +3,54 @@ from stacks_queues import Stack
 
 class DirectionalGraph(object):
     def __init__(self):
-        self.adj = {}
-        self.next = 0
+        self.adj: dict = {}
         self.size = 0
 
-    def get_neighbors(self, i):
-        return self.adj[i]
-
-    def get_edges_for(self, i):
-        return [(i, j) for j in self.adj[i]]
-
-    def add_edge(self, i, j):
-        self.adj[i].append(j)
-
-    def remove_edge(self, i, j):
-        if j in self.adj[i]:
-            self.adj[i].remove(j)
-        if i in self.adj[j]:
-            self.adj[j].remove(i)
-
-    def new_vertex(self) -> int:
-        i = self.next
-        self.adj[i] = []
+    def add_vertex(self, key):
+        self.adj[key] = []
         self.size += 1
-        self.next += 1
-        return i
 
-    def remove_vertex(self, i):
-        self.size -= 1
-        for edge in self.get_edges_for(i):
-            a, b = edge
-            self.remove_edge(a, b)
-        del self.adj[i]
+    def __len__(self) -> int:
+        return self.size
 
-    def has_path(self, i, j):
-        # DFS uses a Stack, BFS uses a Queue
-        dfs = Stack()
-        searched = []
-        dfs.push(i)
-        while len(dfs) > 0:
-            cur = dfs.pop()
-            for ne in self.get_neighbors(cur):
-                if ne == j:
-                    return True
-                elif ne not in searched:
-                    dfs.push(ne)
-            searched.append(cur)
-        return False
+    def get_neighbors(self, key) -> list:
+        return self.adj[key]
+
+    def add_edge(self, key_1, key_2):
+        self.adj[key_1].append(key_2)
+
+    def has_path(self, key_1, key_2):
+        return len(self.get_path(key_1, key_2)) > 0
+
+    def get_path(self, key_1, key_2) -> list:
+        # DFS uses Stack, BFS uses Queue
+        stack = Stack()
+        visited = []
+        stack.push((key_1, []))
+        while len(stack):
+            cur, path = stack.pop()
+            path.append(cur)
+            for nei in self.get_neighbors(cur):
+                if nei == key_2:
+                    path.append(nei)
+                    return path
+                elif nei not in visited:
+                    stack.push((nei, [_ for _ in path]))
+                    visited.append(nei)
+        return []
 
 
 if __name__ == '__main__':
     g = DirectionalGraph()
-    g.new_vertex()
-    g.new_vertex()
-    g.new_vertex()
-    g.new_vertex()
-    g.new_vertex()
+    g.add_vertex(0)
+    g.add_vertex(1)
+    g.add_vertex(2)
+    g.add_vertex(3)
+    g.add_vertex(4)
     g.add_edge(0, 1)
     g.add_edge(1, 2)
     g.add_edge(3, 2)
     g.add_edge(2, 4)
     print(g.has_path(0, 4))
     print(g.has_path(0, 3))
+    print(g.get_path(0, 4))
